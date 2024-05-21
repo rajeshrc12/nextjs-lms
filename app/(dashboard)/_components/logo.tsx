e9feee277aed553e2b57bec1aabb99d85ae59cc2
rajeshrc12 <rajesh.charhajari@gmail.com>
2024-05-21 14:35:49 +0530

    course creation ui

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const Logo = () => {
-  return <Image src={"/next.svg"} height={130} width={130} alt="logo" />;
+  return <Image src={"/logo.svg"} height={130} width={130} alt="logo" />;
 };
 
 export default Logo;
