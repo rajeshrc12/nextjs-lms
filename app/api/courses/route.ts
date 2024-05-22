2650f091d2bac1f6ab9da5cf7a5a074d71c9eb8e
rajeshrc12 <rajesh.charhajari@gmail.com>
2024-05-22 16:09:22 +0530

    course api creation

---
+++ app/api/courses/route.ts
@@ -0,0 +1,19 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const { title } = await req.json();
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const course = await db.course.create({ data: { title, userId } });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSES]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
