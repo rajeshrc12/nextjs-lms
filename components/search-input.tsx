e1d18c2364329fe5a9a2a7a07d3f9fa39095328f
rajeshrc12 <rajesh.charhajari@gmail.com>
2024-05-26 18:59:31 +0530

    search query

---
+++ components/search-input.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import qs from "query-string";
+const SearchInput = () => {
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce(value);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const currentCategoryId = searchParams.get("categoryId");
+
+  useEffect(() => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          title: debouncedValue,
+          categoryId: currentCategoryId,
+        },
+      },
+      {
+        skipNull: true,
+        skipEmptyString: true,
+      }
+    );
+
+    router.push(url);
+  }, [debouncedValue, currentCategoryId, router, pathname]);
+
+  return (
+    <div className="relative">
+      <Search className="absolute top-3 left-3 h-4 w-4 text-slate-600" />
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
+        placeholder="Search for a course"
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
