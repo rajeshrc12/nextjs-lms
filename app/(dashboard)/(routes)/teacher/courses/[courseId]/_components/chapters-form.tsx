bb35751af49ba4a3c7c418a7edb84d0ac8d48d81
rajeshrc12 <rajesh.charhajari@gmail.com>
2024-05-26 17:41:36 +0530

    chapter loader added

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -16,7 +16,7 @@ import { Chapter, Course } from "@prisma/client";
 import axios from "axios";
 import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import * as z from "zod";
@@ -34,6 +34,7 @@ const formSchema = z.object({
 const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
   const [isCreating, setIsCreating] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
+  const [chapters, setChapters] = useState<any>(null);
   const router = useRouter();
   const toggleCreating = () => setIsCreating((current) => !current);
 
@@ -74,9 +75,13 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
   const onEdit = (id: string) => {
     router.push(`/teacher/courses/${courseId}/chapters/${id}`);
   };
+
+  useEffect(() => {
+    setChapters(initialData.chapters);
+  }, [initialData.chapters]);
   return (
     <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
-      {isUpdating && (
+      {(isUpdating || chapters === null) && (
         <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
           <Loader2 className="animate-spin h-6 w-6 text-sky-700k" />
         </div>
@@ -126,14 +131,14 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
         <div
           className={cn(
             "text-sm mt-2",
-            !initialData.chapters.length && "text-slate-500 italic"
+            !chapters?.length && "text-slate-500 italic"
           )}
         >
-          {!initialData.chapters.length && "No chapters"}
+          {!chapters?.length && "No chapters"}
           <ChaptersList
             onEdit={onEdit}
             onReorder={onReorder}
-            items={initialData.chapters || []}
+            items={chapters || []}
           />
         </div>
       )}
